ac5ee363353084c96c8f541021065f2aabd1f1eb
Mauricio Barbosa <37017091+M4urici0GM@users.noreply.github.com>
2024-03-24 17:28:27 -0300

    improved the code

--- lua/netcoredbg-macOS-arm64/init.lua
+++ lua/netcoredbg-macOS-arm64/init.lua
@@ -3,80 +3,82 @@ local M = {}
 
 
 local function get_plugin_directory()
-  local str = debug.getinfo(1, "S").source:sub(2)
-  str = str:match("(.*/)")               -- Get the directory of the current file
-  return str:gsub("/[^/]+/[^/]+/$", "/") -- Go up two directories
+    local str = debug.getinfo(1, "S").source:sub(2)
+    str = str:match("(.*/)")               -- Get the directory of the current file
+    return str:gsub("/[^/]+/[^/]+/$", "/") -- Go up two directories
 end
 
 local plugin_directory = get_plugin_directory()
 local netcoredbg_path = plugin_directory .. 'netcoredbg/netcoredbg'
 
-
 M.setup = function(dap)
-  dap.adapters.coreclr = {
-    type = 'executable',
-    command = netcoredbg_path,
-    args = { '--interpreter=vscode' }
-  }
+    dap.adapters.coreclr = {
+        type = 'executable',
+        command = netcoredbg_path,
+        args = { '--interpreter=vscode' }
+    }
 
-  local function getCurrentFileDirName()
-    local fullPath = vim.fn.expand('%:p:h')      -- Get the full path of the directory containing the current file
-    local dirName = fullPath:match("([^/\\]+)$") -- Extract the directory name
-    return dirName
-  end
+    local function dotnet_build_project ()
+        local default_path = vim.fn.getcwd() .. '/'
+        if vim.g['dotnet_last_proj_path'] ~= nil then
+            default_path = vim.g['dotnet_last_proj_path']
+        end
 
-  local function file_exists(name)
-    local f = io.open(name, "r")
-    if f ~= nil then
-      io.close(f)
-      return true
-    else
-      return false
+        local path = vim.fn.input('Path to your *proj file', default_path, 'file')
+        vim.g['dotnet_last_proj_path'] = path
+        local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
+        print('')
+        print('Cmd to execute: ' .. cmd)
+        local f = os.execute(cmd)
+        if f == 0 then
+            print('\nBuild: ✔️ ')
+        else
+            print('\nBuild: ❌ (code: ' .. f .. ')')
+        end
     end
-  end
 
-  local function get_dll_path()
-    local debugPath = vim.fn.expand('%:p:h') .. '/bin/Debug'
-    if not file_exists(debugPath) then
-      return vim.fn.getcwd()
-    end
-    local command = 'find "' .. debugPath .. '" -maxdepth 1 -type d -name "*net*" -print -quit'
-    local handle = io.popen(command)
-    local result = handle:read("*a")
-    handle:close()
-    result = result:gsub("[\r\n]+$", "") -- Remove trailing newline and carriage return
-    if result == "" then
-      return debugPath
-    else
-      local potentialDllPath = result .. '/' .. getCurrentFileDirName() .. '.dll'
-      if file_exists(potentialDllPath) then
-        return potentialDllPath
-      else
-        return result == "" and debugPath or result .. '/'
-      end
-      --        return result .. '/' -- Adds a trailing slash if a net folder is found
-    end
-  end
+    local  function dotnet_get_dll_path ()
+        local request = function()
+            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        end
 
-  dap.configurations.cs = {
-    {
-      type = 'coreclr',
-      name = 'NetCoreDbg: Launch',
-      request = 'launch',
-      cwd = '${fileDirname}',
-      program = function()
-        return vim.fn.input('Path to dll', get_dll_path(), 'file')
-      end,
-      env = {
-        ASPNETCORE_ENVIRONMENT = function()
-          return vim.fn.input("ASPNETCORE_ENVIRONMENT: ", "Development")
-        end,
-        ASPNETCORE_URL = function()
-          return vim.fn.input("ASPNETCORE_URL: ", "http://localhost:5000")
-        end,
-      }
-    },
-  }
+        if vim.g['dotnet_last_dll_path'] == nil then
+            vim.g['dotnet_last_dll_path'] = request()
+        else
+            if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
+                vim.g['dotnet_last_dll_path'] = request()
+            end
+        end
+
+        return vim.g['dotnet_last_dll_path']
+    end
+
+
+
+    dap.configurations.cs = {
+        {
+            type = 'coreclr',
+            name = 'NetCoreDbg: Launch',
+            request = 'launch',
+            cwd = '${fileDirname}',
+            console = "integratedTerminal",
+            program = function()
+                if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+                    dotnet_build_project()
+                end
+
+                return dotnet_get_dll_path()
+            end,
+            env = {
+                ASPNETCORE_ENVIRONMENT = function()
+                    return vim.fn.input("ASPNETCORE_ENVIRONMENT: ", "Development")
+                end,
+                ASPNETCORE_URL = function()
+                    return vim.fn.input("ASPNETCORE_URL: ", "http://localhost:5000")
+                end,
+            }
+        },
+    }
 end
 
 return M
